4d93e2333341af78f7076cf695a0166ece1c2925
hua5155 <s17115155@stu.edu.tw>
2023-11-28 16:13:42 +0800

    Added toggle switch UI component. Refactored Tic Tac Toe, ability to undo & redo

---
+++ src/lib/ToggleSwitch.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let switchState = false;
+</script>
+
+<button
+	class="inline-flex gap-4"
+	on:click={() => {
+		switchState = !switchState;
+	}}
+>
+	<slot name="left-text" />
+	<div
+		class="inline-flex h-6 w-11 rounded-full p-[2px] transition-colors duration-300"
+		class:bg-gray-400={!switchState}
+		class:bg-green-500={switchState}
+	>
+		<div
+			class="h-5 w-5 rounded-full bg-white transition-transform duration-300"
+			class:translate-x-full={switchState}
+		/>
+	</div>
+	<slot name="right-text" />
+</button>
--- src/routes/tic-tac-toe/+page.svelte
+++ src/routes/tic-tac-toe/+page.svelte
@@ -4,8 +4,9 @@
 	import z from 'zod';
 
 	import { setContext } from 'svelte';
+	import ToggleSwitch from '$lib/ToggleSwitch.svelte';
 
-	function calculateWinner(grid: string[]) {
+	function calculateWinner(grid: string[]): GameStatus {
 		const winCondition = [
 			[0, 1, 2], // top      -
 			[3, 4, 5], // mid      -
@@ -21,45 +22,123 @@
 			const [a, b, c] = line;
 			if (grid[a] !== '' && grid[a] === grid[b] && grid[a] === grid[c]) {
 				if (debugMode) console.log(`winner is : ${grid[a]}!`);
-				return;
+				if (grid[a] === 'O') return 'winner is O';
+				if (grid[a] === 'X') return 'winner is X';
 			}
 		}
 
 		if (move === 9) {
 			if (debugMode) console.log('draw');
-			return;
+			return 'draw';
 		}
+
 		if (debugMode) console.log('no winner');
+		return '';
 	}
 
-	function handleClick(index: number) {
-		if (xIsNext === true) gridValue[index] = 'X';
-		else gridValue[index] = 'O';
+	function handleClick(position: number) {
+		if (gameStaus !== '') return;
+
+		let girdCopy = [...gridValue]; // use spread syntax so I'm not copying a pointer to array.
+		if (xIsNext === true) girdCopy[position] = 'X';
+		else girdCopy[position] = 'O';
+		move += 1;
+		history.set(move, girdCopy);
 		xIsNext = !xIsNext;
 	}
+
+	function gameReset() {
+		move = 0;
+		history.clear();
+		history.set(
+			move,
+			[...Array(9)].map(() => '')
+		);
+		gridValue = history.get(move)!;
+		xIsNext = move % 2 === 0;
+		gameStaus = '';
+	}
+
+	type GameStatus = 'winner is O' | 'winner is X' | 'draw' | '';
 
 	let debugMode = true;
 	setContext('debugMode', debugMode);
 
 	let move = 0;
-	let gridValue = [...Array(9)].map(() => '');
+	const history = new Map<number, string[]>().set(
+		move,
+		[...Array(9)].map(() => '')
+	);
+	let gridValue = history.get(move)!; // undefined workaround (non-null assertion)
 	let xIsNext = move % 2 === 0;
+	let gameStaus: GameStatus = '';
 
 	$: {
 		move;
+		gridValue = history.get(move)!;
+		gameStaus = calculateWinner(gridValue);
 
-		if (debugMode) console.log('grid : ', gridValue);
-		calculateWinner(gridValue);
+		if (debugMode) {
+			console.log(`move : ${move}/${[...history].length - 1}\n` + `gridValue : ${gridValue}`);
+			// console.log('game status : ', gameStaus);
+		}
 	}
 </script>
 
-<div class="grid w-fit grid-flow-row grid-cols-3">
-	{#each gridValue as value, index}
-		<Grid
-			{value}
+<div class="flex flex-row gap-10">
+	<div class="grid w-fit grid-flow-row grid-cols-3">
+		{#each gridValue as value, index}
+			<Grid
+				{value}
+				on:click={() => {
+					handleClick(index);
+				}}
+			></Grid>
+		{/each}
+	</div>
+
+	<div class="flex flex-col gap-5">
+		<ToggleSwitch bind:switchState={debugMode}>
+			<span slot="left-text">debug mode</span>
+		</ToggleSwitch>
+		<button
+			class="h-fit w-fit rounded-full bg-cyan-500 px-5 pb-1 text-xl hover:bg-cyan-600"
 			on:click={() => {
-				handleClick(index);
+				gameReset();
 			}}
-		></Grid>
-	{/each}
+		>
+			Reset
+		</button>
+		<button
+			class="h-fit w-fit rounded-full bg-cyan-500 px-5 pb-1 text-xl hover:bg-cyan-600"
+			on:click={() => {
+				if (move === 0) return;
+				move -= 1;
+			}}
+		>
+			Undo
+		</button>
+		<button
+			class="h-fit w-fit rounded-full bg-cyan-500 px-5 pb-1 text-xl hover:bg-cyan-600"
+			on:click={() => {
+				if (move === [...history].length - 1) return;
+				move += 1;
+			}}
+		>
+			Redo
+		</button>
+	</div>
 </div>
+
+{#if debugMode}
+	<div>
+		<p>{`moves : ${move}/${[...history].length - 1}`}</p>
+	</div>
+	<div>
+		{#key move}
+			{#each history as record}
+				<p>{`${record[0]} : ${record[1]}`}</p>
+			{/each}
+		{/key}
+	</div>
+{/if}
